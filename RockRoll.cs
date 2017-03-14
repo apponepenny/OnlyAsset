48c698c2cf7d67827b035125ecf469758840cc46
apponepenny <ysendemail@gmail.com>
2017-03-14 17:01:37 +0800

    OOO

---
+++ RockRoll.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockRoll : MonoBehaviour {
+	public GameObject Rock;
+	public float time;
+	public int Size;
+	float t;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	void RandRoll(){
+		
+		GameObject rock = Instantiate (Rock);
+		rock.transform.parent = this.transform;
+		rock.transform.localScale = new Vector3 (Size,Size*1.5f,Size);
+		rock.transform.localPosition = new Vector3 (Random.Range(-30f,30f),0,0);
+		Destroy (rock, 6);
+	
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		if (t > time) {
+			RandRoll ();
+			t = 0;
+		}
+
+
+
+	}
+}
---
+++ destoryMeshCol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destoryMeshCol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameStaticData.isStart) {
+			GetComponent<MeshCollider> ().enabled = false;
+			GetComponent<destoryMeshCol> ().enabled = false;
+		}
+	}
+}
